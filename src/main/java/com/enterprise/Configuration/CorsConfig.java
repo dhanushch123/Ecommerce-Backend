20ba15c2ea43a39544bd12f9d962f68b66815712
dhanushch123 <dhanushch3@gmail.com>
2025-07-29 18:16:55 +0530

    commit

--- src/main/java/com/enterprise/Configuration/CorsConfig.java
+++ src/main/java/com/enterprise/Configuration/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("https://ecommerce-frontend-dhanushch123s-projects.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
